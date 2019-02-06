b71fe5ba19251f5756e1477d395cbacb7e18e3ef
Donald <DonaldET3@gmail.com>
2019-02-06 15:50:16 -0800

    Add files via upload

--- break.c
+++ break.c
@@ -1,69 +1,179 @@
 /* Opal file breaker
- * for POSIX
- * version 1
+ * for Unix
+ * version 2
  */
 
+
+/* pieces section */
+
 #include <stdio.h>
+/* getc()
+ * putc()
+ * sscanf()
+ * fprintf()
+ * perror()
+ * fopen()
+ * fclose()
+ * FILE
+ * NULL
+ * EOF
+ */
+
 #include <stdlib.h>
+/* exit()
+ * NULL
+ * EXIT_FAILURE
+ * EXIT_SUCCESS
+ */
+
 #include <string.h>
+/* strlen()
+ */
+
+#include <stdbool.h>
+/* bool
+ * true
+ * false
+ */
+
+#include <unistd.h>
+/* getopt()
+ */
+
+
+/* functions section */
+
+/* help message */
+void help()
+{
+ char *message = "Opal file breaker\n"
+ "version 2\n\n"
+ "options\n"
+ "h: print help and exit\n"
+ "f: specify file to break (default: standard input)\n"
+ "s: maximum size of each output file (required)\n"
+ "u: unit scale (K, M, G, T, or P)\n"
+ "a: first part of output file names\n"
+ "d: minimum number of digits in the file number\n"
+ "z: last part of output file names\n";
+ fputs(message, stderr);
+}
+
+/* library error */
+void error(char *message)
+{
+ perror(message);
+ exit(EXIT_FAILURE);
+}
+
+/* internal failure */
+void fail(char *message)
+{
+ fprintf(stderr, "%s\n", message);
+ exit(EXIT_FAILURE);
+}
+
+/* invalid command line argument */
+void invalid(char c)
+{
+ fprintf(stderr, "argument supplied to -%c is invalid\n", c);
+ exit(EXIT_FAILURE);
+}
 
 int main(int argc, char **argv)
 {
  int c;
- unsigned i, dc = 0, fnum = 1, limit = 0;
- long long unsigned bnum, bmax;
- char a[128], z[128], fname[256], fmt[32], *sp;
- FILE *fp;
+ unsigned f_num = 0, digits = 1;
+ long long unsigned size = 0, position;
+ char unit, *inn, *outn, *outn_a, *outn_z, *fmt, nc;
+ FILE *inf, *outf;
+ extern char *optarg;
+ extern int opterr, optind, optopt;
 
- if(argc < 3)
- {
-  fputs("Missing argument\n", stderr);
-  exit(EXIT_FAILURE);
- }
+ inf = stdin;
+ inn = outn_a = outn_z = outn = NULL;
+ unit = nc = '\0';
 
- sscanf(argv[1], "%llu", &bmax);
-
- strncpy(a, argv[2], 127);
- a[127] = '\0';
- sp = strchr(a, '#');
-
- if(sp != NULL)
- {
-  dc = strspn(sp, "#");
-  strcpy(z, strrchr(a, '#') + 1);
-  *sp = '\0';
- }
- else
- {
-  z[0] = '\0';
- }
-
- sprintf(fmt, "%%s%%0%uu%%s", dc);
-
- limit = ~limit;
- c = getchar();
- while((c != EOF) && (fnum < limit))
- {
-  sprintf(fname, fmt, a, fnum, z);
-  fp = fopen(fname, "wb");
-
-  if(fp == NULL)
+ /* parse the command line */
+ while((c = getopt(argc, argv, "hs:u:f:a:z:d:")) != -1)
+  switch(c)
   {
-   fprintf(stderr, "Error: cannot not write file: %s\n", fname);
-   exit(EXIT_FAILURE);
+   case 'h': help(); exit(EXIT_SUCCESS);
+   case 's': if(sscanf(optarg, "%llu", &size) != 1) invalid(c); break;
+   case 'u': unit = *optarg; break;
+   case 'f': inn = optarg; break;
+   case 'a': outn_a = optarg; break;
+   case 'z': outn_z = optarg; break;
+   case 'd': if(sscanf(optarg, "%u", &digits) != 1) invalid(c); break;
+   case '?': exit(EXIT_FAILURE);
   }
 
-  bnum = 0;
-  while((c != EOF) && (bnum < bmax))
-  {
-   putc(c, fp);
-   bnum++;
-   c = getchar();
-  }
+ /* check input values */
+ if(size < 1) fail("\"s\" must be greater than zero");
+ if(digits < 1) fail("\"d\" must be greater than zero");
 
-  fclose(fp);
-  fnum++;
+ /* multiply size by unit */
+ switch(unit)
+ {
+  case 'k': size *= 1000; break;
+  case 'K': size *= 1000; break;
+  case 'm': size *= 1000000; break;
+  case 'M': size *= 1000000; break;
+  case 'g': size *= 1000000000; break;
+  case 'G': size *= 1000000000; break;
+  case 't': size *= 1000000000000; break;
+  case 'T': size *= 1000000000000; break;
+  case 'p': size *= 1000000000000000; break;
+  case 'P': size *= 1000000000000000; break;
+  case '\0': break;
+  default: fail("unrecognized unit");
  }
 
- return 0;
+ /* setup input file */
+ if(inn != NULL)
+  if((inf = fopen(inn, "rb")) == NULL)
+   error(inn);
+
+ /* if no output name is provided */
+ if(outn_a == NULL) outn_a = &nc;
+ if(outn_z == NULL) outn_z = &nc;
+
+ /* allocate space for string processing */
+ if((outn = malloc(strlen(outn_a) + (sizeof(unsigned) / 2) + strlen(outn_z) + 1)) == NULL) error("allocate output name");
+ if((fmt = malloc((sizeof(unsigned) / 2) + 8)) == NULL) error("allocate format string");
+
+ /* prepare format string */
+ sprintf(fmt, "%%s%%0%uu%%s", digits);
+
+ c = 0;
+ /* break the file */
+ while(c != EOF)
+ {
+  /* increment the file number */
+  if(!(++f_num)) fail("too many output files");
+  /* put together the output file name */
+  sprintf(outn, fmt, outn_a, f_num, outn_z);
+  /* open the output file */
+  if((outf = fopen(outn, "wb")) == NULL) error(outn);
+
+  /* copy no more than the specified number of bytes per file */
+  for(position = 0; position < size; position++)
+  {
+   if((c = getc(inf)) == EOF) break;
+   if(putc(c, outf) == EOF) error(outn);
+  }
+
+  /* close the output file */
+  fclose(outf);
+ }
+
+ /* free the strings */
+ free(outn);
+ free(fmt);
+
+ /* close the input file */
+ if(inn != NULL)
+  fclose(inf);
+
+ return EXIT_SUCCESS;
 }
--- merge.c
+++ merge.c
@@ -1,59 +1,156 @@
 /* Opal file merger
- * for POSIX
- * version 1
+ * for Unix
+ * version 2
  */
 
+
+/* pieces section */
+
 #include <stdio.h>
+/* getc()
+ * putc()
+ * sscanf()
+ * fprintf()
+ * perror()
+ * fopen()
+ * fclose()
+ * FILE
+ * NULL
+ * EOF
+ */
+
 #include <stdlib.h>
+/* exit()
+ * NULL
+ * EXIT_FAILURE
+ * EXIT_SUCCESS
+ */
+
 #include <string.h>
+/* strlen()
+ */
+
+#include <stdbool.h>
+/* bool
+ * true
+ * false
+ */
+
+#include <unistd.h>
+/* getopt()
+ */
+
+
+/* functions section */
+
+/* help message */
+void help()
+{
+ char *message = "Opal file merger\n"
+ "version 2\n\n"
+ "options\n"
+ "h: print help and exit\n"
+ "f: specify output file name (default: standard output)\n"
+ "a: first part of input file names\n"
+ "d: minimum number of digits in the file number\n"
+ "z: last part of input file names\n";
+ fputs(message, stderr);
+}
+
+/* library error */
+void error(char *message)
+{
+ perror(message);
+ exit(EXIT_FAILURE);
+}
+
+/* internal failure */
+void fail(char *message)
+{
+ fprintf(stderr, "%s\n", message);
+ exit(EXIT_FAILURE);
+}
+
+/* invalid command line argument */
+void invalid(char c)
+{
+ fprintf(stderr, "argument supplied to -%c is invalid\n", c);
+ exit(EXIT_FAILURE);
+}
 
 int main(int argc, char **argv)
 {
  int c;
- unsigned i, dc = 0, fnum = 1, limit = 0;
- char a[128], z[128], fname[256], fmt[32], *sp;
- FILE *fp;
+ unsigned f_num = 0, digits = 1;
+ char *inn, *outn, *inn_a, *inn_z, *fmt, nc;
+ FILE *inf, *outf;
+ extern char *optarg;
+ extern int opterr, optind, optopt;
 
- if(argc < 2)
- {
-  fputs("Error: missing argument\n", stderr);
-  exit(EXIT_FAILURE);
- }
+ outf = stdout;
+ inn = inn_a = inn_z = outn = NULL;
+ nc = '\0';
 
- strncpy(a, argv[1], 127);
- a[127] = '\0';
- sp = strchr(a, '#');
-
- if(sp != NULL)
- {
-  dc = strspn(sp, "#");
-  strcpy(z, strrchr(a, '#') + 1);
-  *sp = '\0';
- }
- else
- {
-  z[0] = '\0';
- }
-
- sprintf(fmt, "%%s%%0%uu%%s", dc);
-
- limit = ~limit;
- sprintf(fname, fmt, a, fnum, z);
- fp = fopen(fname, "rb");
- while((fp != NULL) && (fnum < limit))
- {
-  c = getc(fp);
-  while(c != EOF)
+ /* parse the command line */
+ while((c = getopt(argc, argv, "hf:a:z:d:")) != -1)
+  switch(c)
   {
-   putchar(c);
-   c = getc(fp);
+   case 'h': help(); exit(EXIT_SUCCESS);
+   case 'f': outn = optarg; break;
+   case 'a': inn_a = optarg; break;
+   case 'z': inn_z = optarg; break;
+   case 'd': if(sscanf(optarg, "%u", &digits) != 1) invalid(c); break;
+   case '?': exit(EXIT_FAILURE);
   }
 
-  fclose(fp);
-  fnum++;
-  sprintf(fname, fmt, a, fnum, z);
-  fp = fopen(fname, "rb");
+ /* check input values */
+ if(digits < 1) fail("\"d\" must be greater than zero");
+
+ /* setup output file */
+ if(outn != NULL)
+  if((outf = fopen(outn, "wb")) == NULL)
+   error(outn);
+
+ /* if no input name is provided */
+ if(inn_a == NULL) inn_a = &nc;
+ if(inn_z == NULL) inn_z = &nc;
+
+ /* allocate space for string processing */
+ if((inn = malloc(strlen(inn_a) + (sizeof(unsigned) / 2) + strlen(inn_z) + 1)) == NULL) error("allocate input name");
+ if((fmt = malloc((sizeof(unsigned) / 2) + 8)) == NULL) error("allocate format string");
+
+ /* prepare format string */
+ sprintf(fmt, "%%s%%0%uu%%s", digits);
+
+ /* merge the files */
+ while(true)
+ {
+  /* increment the file number */
+  if(!(++f_num)) fail("too many output files");
+  /* put together the input file name */
+  sprintf(inn, fmt, inn_a, f_num, inn_z);
+  /* open the input file */
+  if((inf = fopen(inn, "rb")) == NULL) break;
+
+  c = 0;
+  /* copy all the bytes */
+  while(c != EOF)
+  {
+   if((c = getc(inf)) == EOF) break;
+   if(putc(c, outf) == EOF) error(outn);
+  }
+
+  /* close the output file */
+  fclose(inf);
  }
 
- return 0;
+ /* free the strings */
+ free(inn);
+ free(fmt);
+
+ /* close the input file */
+ if(outn != NULL)
+  fclose(outf);
+
+ return EXIT_SUCCESS;
 }
